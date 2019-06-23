806721115717aec13446c18135a2ca8fd1a47509
Your Name <you@example.com>
2019-06-23 06:18:49 -0300

    loop

--- pof.py
+++ pof.py
@@ -40,26 +40,29 @@ POFobject.login( )
 print('logged in')
 galleryData = list()
 print('list')
+import time
+while True:
 
-users = POFobject.searchUsers(config, 100, online_only=True)
-print("Search complete.")
+    users = POFobject.searchUsers(config, 100, online_only=True)
+    print("Search complete.")
 
-tomsg = []
-for user in users:
-    try:
-        #print (user.profile_url)
-        #print(user.description.encode("utf-8"))
-        if(sentiment_analyzer_scores(user.description.encode("utf-8"))['compound'] > 0.985):
-            if user.online == True:
-                if user.profile_url not in usersDone:
-                    usersDone.append(user.profile_url)
-                    tomsg.append(user)
-            
-    except Exception as e:
-        print (e)
-print("Total Users Found: {0}".format( len(tomsg) ) )
-with open('usersDone.txt', 'w') as f:
-    for item in usersDone:
-        f.write("%s\n" % item)
+    tomsg = []
+    for user in users:
+        try:
+            #print (user.profile_url)
+            #print(user.description.encode("utf-8"))
+            if(sentiment_analyzer_scores(user.description.encode("utf-8"))['compound'] > 0.99):
+                if user.online == True:
+                    if user.profile_url not in usersDone:
+                        usersDone.append(user.profile_url)
+                        tomsg.append(user)
+                
+        except Exception as e:
+            print (e)
+    print("Total Users Found: {0}".format( len(tomsg) ) )
+    with open('usersDone.txt', 'w') as f:
+        for item in usersDone:
+            f.write("%s\n" % item)
 
-#broadcastMessage(tomsg, "Hey! This is going to sound terrrrribly awkward but I hacked POF in a very special way that allows me to rank different profiles among the online users to see whose description matches an analysis I'd created that might indicate we'd have a swell chance at a good conversation - so if this isn't terribly scary as well as terribly awkward, do hazard a response back and we'll see if I'm right?")
+    POFobject.broadcastMessage(tomsg, "Hey! This is going to sound terrrrribly awkward but I hacked POF in a very special way that allows me to rank different profiles among the online users to see whose description matches an analysis I'd created that might indicate we'd have a swell chance at a good conversation - so if this isn't terribly scary as well as terribly awkward, do hazard a response back and we'll see if I'm right? I'm legit though here's the code: https://github.com/DunnCreativeSS/POFHacking" )
+    time.sleep(60)
