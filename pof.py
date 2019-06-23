b78bca4204d25017fd15b3ac7882415c67a27275
Your Name <you@example.com>
2019-06-23 06:57:25 -0300

    correction...

--- pof.py
+++ pof.py
@@ -43,7 +43,7 @@ print('list')
 import time
 while True:
 
-    users = POFobject.searchUsers(config, 100, online_only=True)
+    users = POFobject.searchUsers(config, 20, online_only=True)
     print("Search complete.")
 
     tomsg = []
@@ -65,4 +65,8 @@ while True:
             f.write("%s\n" % item)
 
     POFobject.broadcastMessage(tomsg, "Hey! This is going to sound terrrrribly awkward but I hacked POF in a very special way that allows me to rank different profiles among the online users to see whose description matches an analysis I'd created that might indicate we'd have a swell chance at a good conversation - so if this isn't terribly scary as well as terribly awkward, do hazard a response back and we'll see if I'm right? I'm legit though here's the code: https://github.com/DunnCreativeSS/POFHacking - in case my account gets auto-shutdown after much of this, I'll drop my American cell # here: 17162268165" )
-    time.sleep(60)
+    if len(tomsg) > 1:
+        time.sleep(20)
+    elif len(tomsg) == 1:
+        time.sleep(5)
+        
